9c8399d31a662e66140b1c9545e4d2b1c67a9fba
noah <nojia.adjemrose@gmail.com>
2024-11-12 17:20:12 +0100

    ft_strtrim.c

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -3,98 +3,37 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strtrim.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: nadjemia <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: nadjemia <nadjemia@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/11 15:51:58 by nadjemia          #+#    #+#             */
-/*   Updated: 2023/11/12 13:58:59 by nadjemia         ###   ########.fr       */
+/*   Updated: 2024/11/12 17:19:28 by nadjemia         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-typedef struct s_coor	t_coor;
-
-typedef struct s_coor
-{
-	int	d;
-	int	f;
-}		t_coor;
-
-static void	ft_size(const char *str, const char *set, t_coor *coor);
-static int	ft_isset(char c, const char *set);
-static char	*ft_fill(char *str, const char *s1, t_coor *coor, const char *set);
-
 char	*ft_strtrim(char const *s1, char const *set)
 {
-	t_coor	coor;
 	char	*str;
-
-	coor.d = 0;
-	coor.f = 0;
-	ft_size(s1, set, &coor);
-	if (coor.d > coor.f)
-	{
-		str = malloc(sizeof(char));
-		*str = '\0';
-		return (str);
-	}
-	str = malloc(sizeof(char) * (coor.f - coor.d + 2));
-	if (!str)
+	int		i;
+	
+	if (s1 == NULL || set == NULL)
 		return (NULL);
-	ft_fill(str, s1, &coor, set);
-	return (str);
-}
-
-static void	ft_size(char const *str, const char *set, t_coor *coor)
-{
-	size_t	i;
-
-	i = 0;
-	while (ft_isset(str[i], set))
-		i++;
-	coor->d = i;
-	i = ft_strlen(str) - 1;
-	while (ft_isset(str[i], set))
-		i--;
-	coor->f = i;
-}
-
-static int	ft_isset(char c, const char *set)
-{
-	while (*set)
+	str = ft_strdup(s1);
+	i = ft_strlen(s1);
+	while (--i >= 0)
 	{
-		if (c == *set)
-			return (1);
-		set++;
-	}
-	return (0);
-}
-
-static char	*ft_fill(char *str, const char *s1, t_coor *coor, const char *set)
-{
-	int	i;
-
-	i = 0;
-	if (coor->d == coor->f)
-		coor->f += 1;
-	if (!ft_isset(s1[coor->f], set))
-	{
-		while (coor->d <= coor->f)
+		if (!ft_strchr(set, s1[i]))
 		{
-			str[i] = s1[coor->d];
-			i++;
-			coor->d += 1;
+			str[i + 1] = '\0';
+			break ;
 		}
 	}
-	else
+	while (ft_strchr(set, str[0]))
 	{
-		while (coor->d < coor->f)
-		{
-			str[i] = s1[coor->d];
-			i++;
-			coor->d += 1;
-		}
+		i = -1;
+		while (str[++i])
+			str[i] = str[i + 1];
 	}
-	str[i] = '\0';
 	return (str);
 }
